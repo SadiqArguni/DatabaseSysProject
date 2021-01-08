c2a985ab209ade2d557e9d0fdd8f94edab968e14
SadiqArguni <sadiq.arguni96@gmail.com>
2021-01-08 17:11:02 +0500

    commit check

---
+++ Pizza_Service_Project/controller/getCustomers.php
@@ -0,0 +1,26 @@
+<?php
+
+        require_once("Records.php");
+        $records = new Records();
+
+        $customerRecords = $records->getAllRecords("Customers");
+
+        $name = array();
+        $address = array();
+        $city = array();
+        $province = array();
+        $postalCode = array();
+        $email = array();
+
+        if($customerRecords != null)
+        foreach($customerRecords as $customers){
+            array_push($name, $customers['name']);
+            array_push($address, $customers['address']);
+            array_push($city, $customers['city']);
+            array_push($province, $customers['province']);
+            array_push($postalCode, $customers['postalCode']);
+            array_push($email, $customers['email']);
+        }
+
+        $username = $name[0];
+   
